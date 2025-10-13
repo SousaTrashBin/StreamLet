bac0cda3d91b3bd3dd0a087ae098270793498f45
Diogo Sousa <sousanamain@gmail.com>
2025-10-13 16:26:01 +0100

    fix, no need to use the byte buffer, can use the block directly since hashcode is overwritten

--- src/application/BlockchainManager.java
+++ src/application/BlockchainManager.java
@@ -3,37 +3,32 @@ package application;
 import utils.application.Block;
 import utils.application.Transaction;
 
-import java.nio.ByteBuffer;
 import java.util.*;
 
 public class BlockchainManager {
     private static final int SHA1_LENGTH = 20;
     private static final Block GENESIS_BLOCK = new Block(new byte[SHA1_LENGTH], 0, 0, new Transaction[0]);
     private final List<Block> blockChain = new ArrayList<>();
-    private final Map<ByteBuffer, Boolean> blockNotarizationStatus = new HashMap<>();
+    private final Map<Block, Boolean> blockNotarizationStatus = new HashMap<>();
     private final Set<Block> finalizedBlocks = new HashSet<>();
 
     public BlockchainManager() {
         blockChain.add(GENESIS_BLOCK);
-        ByteBuffer hashBuffer = ByteBuffer.wrap(GENESIS_BLOCK.hash());
-        blockNotarizationStatus.put(hashBuffer, false);
+        blockNotarizationStatus.put(GENESIS_BLOCK, false);
     }
 
     public void addBlock(Block newBlock) {
         blockChain.add(newBlock);
-        ByteBuffer hashBuffer = ByteBuffer.wrap(newBlock.hash());
-        blockNotarizationStatus.put(hashBuffer, false);
+        blockNotarizationStatus.put(newBlock, false);
     }
 
     public void notarizeBlock(Block block) {
-        ByteBuffer hashBuffer = ByteBuffer.wrap(block.hash());
-        blockNotarizationStatus.put(hashBuffer, true);
-
+        blockNotarizationStatus.replace(block, false, true);
         finalizeConsecutiveBlocks(block);
     }
 
     public boolean isNotarized(Block block) {
-        return blockNotarizationStatus.getOrDefault(ByteBuffer.wrap(block.hash()), false);
+        return blockNotarizationStatus.getOrDefault(block, false);
     }
 
     public Block getLongestNotarizedChainTip() {
