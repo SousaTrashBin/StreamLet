92a16d21f1fca94f30ac5ab75bf87586f8d7dd37
Diogo Sousa <sousanamain@gmail.com>
2025-10-15 18:21:42 +0100

    fix stream let node implementation

--- src/application/StreamletNode.java
+++ src/application/StreamletNode.java
@@ -13,10 +13,14 @@ import java.util.*;
 import java.util.concurrent.*;
 import java.util.concurrent.atomic.AtomicInteger;
 
+record SeenProposalKey(int leader, int epoch) {
+
+}
+
 public class StreamletNode {
 
-    private final static int CONFUSION_START = 30;
-    private final static int CONFUSION_DURATION = 5;
+    private final static int CONFUSION_START = 8;
+    private final static int CONFUSION_DURATION = 12;
     private final URBNode urbNode;
     private final int deltaInSeconds;
     private final int numberOfDistinctNodes;
@@ -28,6 +32,7 @@ public class StreamletNode {
     private final BlockingQueue<Message> derivableQueue = new LinkedBlockingQueue<>();
     private final AtomicInteger currentEpoch = new AtomicInteger(0);
     private final ExecutorService messageExecutor = Executors.newFixedThreadPool(2);
+    private final Map<SeenProposalKey, Block> seenProposals = new HashMap<>();
     private int currentLeaderId = -1;
 
     public StreamletNode(PeerInfo localPeerInfo, List<PeerInfo> remotePeersInfo, int deltaInSeconds)
@@ -89,14 +94,14 @@ public class StreamletNode {
                     int epoch = currentEpoch.get();
 
                     if (epoch >= CONFUSION_START && epoch < CONFUSION_START + CONFUSION_DURATION + 1) {
-                        System.out.printf("Pausing message processing during confusion %d%n", epoch);
+                        System.out.printf("pausing message processing during confusion %d%n", epoch);
 
                         while (epoch >= CONFUSION_START && epoch < CONFUSION_START + CONFUSION_DURATION + 1) {
                             Thread.sleep(deltaInSeconds * 1000L);
                             epoch = currentEpoch.get();
                         }
 
-                        System.out.printf("Resuming message processing after confusion %d%n", epoch);
+                        System.out.printf("resuming message processing after confusion %d%n", epoch);
                     }
 
                     handleMessageDelivery(message);
@@ -116,7 +121,7 @@ public class StreamletNode {
             transactions[i] = transactionPoolSimulator.generateNewTransaction();
         }
 
-        Block parent = blockchainManager.getLongestNotarizedChainTips().stream().findFirst().get();
+        Block parent = blockchainManager.getNotarizedTips().getFirst();
 
         Block newBlock = new Block(
                 parent.hash(),
@@ -130,48 +135,48 @@ public class StreamletNode {
     }
 
     private void handleMessageDelivery(Message message) {
+        System.out.printf("Delivering Message %s%n", message);
         switch (message.type()) {
-            case PROPOSE -> handlePropose(message, currentEpoch.get());
+            case PROPOSE -> handlePropose(message);
             case VOTE -> handleVote(message);
         }
     }
 
-    private void handlePropose(Message message, int epoch) {
+    private void handlePropose(Message message) {
         Block proposedBlock = (Block) message.content();
 
-        if (message.sender() != currentLeaderId) {
+        SeenProposalKey seenProposalKey = new SeenProposalKey(message.sender(), proposedBlock.epoch());
+        if (seenProposals.containsKey(seenProposalKey)
+                || !blockchainManager.extendNotarizedAnyChainTip(proposedBlock)) {
             return;
         }
 
-        if (!blockchainManager.extendsAnyLongestNotarizedTip(proposedBlock)) {
-            return;
-        }
-
-        votedBlocks.putIfAbsent(proposedBlock, new HashSet<>());
-        votedBlocks.get(proposedBlock).add(localId);
+        seenProposals.put(seenProposalKey, proposedBlock);
+        blockchainManager.addBlock(proposedBlock);
 
         Message voteMessage = new Message(MessageType.VOTE, proposedBlock, localId);
         urbNode.broadcastFromLocal(voteMessage);
 
-        blockchainManager.addBlock(proposedBlock);
     }
 
 
     private void handleVote(Message message) {
         Block votedBlock = (Block) message.content();
+
         votedBlocks.putIfAbsent(votedBlock, new HashSet<>());
         Set<Integer> votesForBlock = votedBlocks.get(votedBlock);
         votesForBlock.add(message.sender());
 
-        if (blockchainManager.extendsAnyLongestNotarizedTip(votedBlock)
+        if (blockchainManager.extendNotarizedAnyChainTip(votedBlock)
                 && votedBlocks.get(votedBlock).size() > numberOfDistinctNodes / 2) {
             blockchainManager.notarizeBlock(votedBlock);
         }
-
-        blockchainManager.addBlock(votedBlock);
     }
 
     private int calculateLeaderId(int epoch) {
+        if (epoch < CONFUSION_START || epoch >= CONFUSION_START + CONFUSION_DURATION + 1) {
+            random.nextInt(numberOfDistinctNodes);
+        }
         return epoch % numberOfDistinctNodes;
     }
 
