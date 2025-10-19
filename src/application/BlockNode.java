343413889deac9d84a1b78e0b4f0d467767e2fea
Diogo Sousa <sousanamain@gmail.com>
2025-10-19 01:01:27 +0100

    add some helper methods

--- src/application/BlockNode.java
+++ src/application/BlockNode.java
@@ -5,7 +5,7 @@ import utils.application.Block;
 import java.util.*;
 
 class BlockNode {
-    final Block block;
+    Block block;
     BlockNode parent;
     final Set<BlockNode> children = new HashSet<>();
     boolean notarized = false;
@@ -16,8 +16,8 @@ class BlockNode {
         this.parent = parent;
     }
 
-    public void addChildren(BlockNode blockNode) {
-        children.add(blockNode);
+    public void addChild(BlockNode child) {
+        children.add(child);
     }
 
     @Override
@@ -33,21 +33,47 @@ class BlockNode {
 
     public List<Block> getNotarizedTips() {
         List<Block> tips = new ArrayList<>();
-        buildNotarizedTipsAux(tips);
+        getNotarizedTipsAux(tips);
         return tips;
     }
 
-    private void buildNotarizedTipsAux(List<Block> tips) {
-        if (!notarized) {
-            return;
-        }
-        if (this.children.isEmpty()) {
+    private void getNotarizedTipsAux(List<Block> tips) {
+        if (!notarized) return;
+        if (children.isEmpty()) {
             tips.add(block);
             return;
         }
-        for (BlockNode blockNode : this.children) {
-            blockNode.buildNotarizedTipsAux(tips);
+        for (BlockNode child : children) {
+            child.getNotarizedTipsAux(tips);
         }
     }
 
+    public void printTree() {
+        System.out.println("\n=== Blockchain Tree ===");
+        StringBuilder output = new StringBuilder();
+        buildTreeString(0, output);
+        System.out.println(output);
+    }
+
+    private void buildTreeString(int depth, StringBuilder output) {
+        final String RESET = "\u001B[0m";
+        final String GREEN = "\u001B[32m";
+        final String YELLOW = "\u001B[33m";
+        String indent = " ".repeat(depth * 2);
+        String color = finalized ? GREEN : (notarized ? YELLOW : RESET);
+
+        output.append(String.format("%s%sBlock[%d-%d]%s%n",
+                indent, color, block.epoch(), block.length(), RESET));
+
+        children.stream()
+                .sorted(Comparator.comparingInt(child -> child.block.epoch()))
+                .forEach(child -> child.buildTreeString(depth + 1, output));
+    }
+
+    public BlockNode copyWithoutParent() {
+        BlockNode copy = new BlockNode(this.block, null);
+        copy.notarized = this.notarized;
+        copy.finalized = this.finalized;
+        return copy;
+    }
 }
