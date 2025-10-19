93dc1214e2beb7fea181c3193410671210463597
Diogo Sousa <sousanamain@gmail.com>
2025-10-19 01:02:56 +0100

    fix hash code verification logic

--- src/application/BlockchainManager.java
+++ src/application/BlockchainManager.java
@@ -8,58 +8,54 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
-record HashKey(byte[] bytes) {
-    @Override
-    public int hashCode() {
-        return Arrays.hashCode(bytes);
-    }
-
-    @Override
-    public boolean equals(Object o) {
-        return o instanceof HashKey(byte[] bytes1) && Arrays.equals(bytes, bytes1);
-    }
-}
-
 public class BlockchainManager {
     private static final int SHA1_LENGTH = 20;
 
     private static final Block GENESIS_BLOCK =
-            new Block(new byte[SHA1_LENGTH], 0, 1, new Transaction[0]);
+            new Block(new byte[SHA1_LENGTH], 0, 0, new Transaction[0]);
 
     private final BlockNode notarizedBlockChain;
+    private final Map<Block, BlockNode> blockToNode = new HashMap<>();
     private final Map<HashKey, BlockNode> hashToNode = new HashMap<>();
 
     public BlockchainManager() {
         notarizedBlockChain = new BlockNode(GENESIS_BLOCK, null);
         notarizedBlockChain.notarized = true;
-        hashToNode.put(new HashKey(GENESIS_BLOCK.hash()), notarizedBlockChain);
+        blockToNode.put(GENESIS_BLOCK, notarizedBlockChain);
+        hashToNode.put(new HashKey(GENESIS_BLOCK.getSHA1()), notarizedBlockChain);
     }
 
     public void notarizeBlock(Block block) {
         BlockNode parent = hashToNode.get(new HashKey(block.parentHash()));
-        BlockNode node = hashToNode.computeIfAbsent(new HashKey(block.hash()), _ -> new BlockNode(block, parent));
-        parent.addChildren(node);
+        if (parent == null) {
+            return;
+        }
+        BlockNode node = blockToNode.get(block);
+        parent.addChild(node);
         node.parent = parent;
         node.notarized = true;
         tryToFinalize(node);
     }
 
-    public void addBlock(Block block) {
-        HashKey blockKey = new HashKey(block.hash());
-        BlockNode blockNode = new BlockNode(block, null);
-        hashToNode.put(blockKey, blockNode);
+    public void addPendingBlock(Block block) {
+        BlockNode node = blockToNode.compute(block, (_, oldNode) -> {
+            if (oldNode == null) return new BlockNode(block, null);
+            oldNode.block = block;
+            return oldNode;
+        });
+
+        hashToNode.put(new HashKey(block.getSHA1()), node);
     }
 
     public boolean extendNotarizedAnyChainTip(Block proposedBlock) {
         return getNotarizedTips().stream()
                 .anyMatch(tip -> proposedBlock.length() > tip.length() &&
-                        Arrays.equals(proposedBlock.parentHash(), tip.hash()));
+                        Arrays.equals(proposedBlock.parentHash(), tip.getSHA1()));
     }
 
     public List<Block> getNotarizedTips() {
         return notarizedBlockChain.getNotarizedTips();
     }
-
 
     private void tryToFinalize(BlockNode b1) {
         BlockNode b2 = b1.parent;
@@ -80,46 +76,44 @@ public class BlockchainManager {
         }
     }
 
-    private BlockNode buildWholeChain() {
-        Map<HashKey, BlockNode> newNodes = new HashMap<>();
-        for (BlockNode oldNode : hashToNode.values()) {
-            BlockNode newNode = new BlockNode(oldNode.block, null);
-            newNode.notarized = oldNode.notarized;
-            newNode.finalized = oldNode.finalized;
-            newNodes.put(new HashKey(newNode.block.hash()), newNode);
+    private BlockNode buildFullChain() {
+        Map<HashKey, BlockNode> newHashMap = new HashMap<>();
+        Map<Block, BlockNode> newIdentityMap = new HashMap<>();
+
+        for (BlockNode oldNode : blockToNode.values()) {
+            BlockNode newNode = oldNode.copyWithoutParent();
+            newHashMap.put(new HashKey(oldNode.block.getSHA1()), newNode);
+
+            newIdentityMap.put(newNode.block, newNode);
         }
 
-        BlockNode root = newNodes.get(new HashKey(GENESIS_BLOCK.hash()));
+        for (BlockNode node : newIdentityMap.values()) {
+            if (node.block == GENESIS_BLOCK) continue;
 
-        for (BlockNode node : newNodes.values()) {
-            if (node.block != GENESIS_BLOCK) {
-                BlockNode parent = newNodes.get(new HashKey(node.block.parentHash()));
+            BlockNode parent = newHashMap.get(new HashKey(node.block.parentHash()));
+            if (parent != null) {
                 node.parent = parent;
-                parent.addChildren(node);
+                parent.addChild(node);
             }
         }
 
-        return root;
+        return newIdentityMap.get(GENESIS_BLOCK);
     }
 
-    public void printLinearBlockchain() {
-        System.out.println("\n=== Blockchain Tree ===");
-        StringBuilder sb = new StringBuilder();
-        printSubtree(buildWholeChain(), 0, sb);
-        System.out.println(sb);
+    public void printBlockchainTree() {
+        BlockNode root = buildFullChain();
+        root.printTree();
     }
 
-    private void printSubtree(BlockNode node, int indent, StringBuilder sb) {
-        final String RESET = "\u001B[0m";
-        final String GREEN = "\u001B[32m";
-        final String YELLOW = "\u001B[33m";
+    record HashKey(byte[] bytes) {
+        @Override
+        public int hashCode() {
+            return Arrays.hashCode(bytes);
+        }
 
-        String prefix = " ".repeat(indent * 2);
-        String color = node.finalized ? GREEN : (node.notarized ? YELLOW : RESET);
-        sb.append(String.format("%s%sBlock[%d-%d]%s%n", prefix, color, node.block.epoch(), node.block.length(), RESET));
-
-        for (BlockNode child : node.children) {
-            printSubtree(child, indent + 1, sb);
+        @Override
+        public boolean equals(Object o) {
+            return o instanceof HashKey(byte[] bytes1) && Arrays.equals(bytes, bytes1);
         }
     }
 }
