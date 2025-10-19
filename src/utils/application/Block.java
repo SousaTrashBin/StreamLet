e9841535a56db0fa6b863833eb872ba0b9066762
Diogo Sousa <sousanamain@gmail.com>
2025-10-19 01:03:36 +0100

    fix hash code verification logic (SHA represents the full hash, hashCode represents a representation of a block, parentHash + epoch + length)

--- src/utils/application/Block.java
+++ src/utils/application/Block.java
@@ -5,35 +5,35 @@ import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
 import java.util.Arrays;
 
-public final class Block implements Content {
-
-    private final byte[] parentHash;
-    private final Integer epoch;
-    private final Integer length;
-    private final Transaction[] transactions;
-    private final byte[] blockHash;
+public record Block(byte[] parentHash, Integer epoch, Integer length, Transaction[] transactions) implements Content {
 
     public Block(byte[] parentHash, Integer epoch, Integer length, Transaction[] transactions) {
         this.parentHash = parentHash;
         this.epoch = epoch;
         this.length = length;
         this.transactions = transactions.clone();
-        this.blockHash = computeSHA1();
     }
 
-    public byte[] hash() {
-        return blockHash;
-    }
-
-    private byte[] computeSHA1() {
+    public byte[] getSHA1() {
         try {
             MessageDigest sha1 = MessageDigest.getInstance("SHA-1");
+
             sha1.update(parentHash);
 
             ByteBuffer buffer = ByteBuffer.allocate(8);
             buffer.putInt(epoch);
             buffer.putInt(length);
             sha1.update(buffer.array());
+
+            for (Transaction transaction : transactions) {
+                ByteBuffer transactionBuffer = ByteBuffer.allocate(20);
+                transactionBuffer.putInt(transaction.id());
+                transactionBuffer.putDouble(transaction.amount());
+                transactionBuffer.putInt(transaction.sender());
+                transactionBuffer.putInt(transaction.receiver());
+                sha1.update(transactionBuffer.array());
+            }
+
             return sha1.digest();
         } catch (NoSuchAlgorithmException e) {
             throw new RuntimeException("SHA-1 not available", e);
@@ -42,29 +42,16 @@ public final class Block implements Content {
 
     @Override
     public boolean equals(Object o) {
-        if (this == o) return true;
-        if (!(o instanceof Block other)) return false;
-        return Arrays.equals(this.blockHash, other.blockHash);
+        if (!(o instanceof Block block)) return false;
+
+        return epoch.equals(block.epoch) && length.equals(block.length) && Arrays.equals(parentHash, block.parentHash);
     }
 
     @Override
     public int hashCode() {
-        return Arrays.hashCode(blockHash);
-    }
-
-    public byte[] parentHash() {
-        return parentHash;
-    }
-
-    public Integer epoch() {
-        return epoch;
-    }
-
-    public Integer length() {
-        return length;
-    }
-
-    public Transaction[] transactions() {
-        return transactions;
+        int result = Arrays.hashCode(parentHash);
+        result = 31 * result + epoch.hashCode();
+        result = 31 * result + length.hashCode();
+        return result;
     }
 }
